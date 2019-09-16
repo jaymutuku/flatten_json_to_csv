f0cf455b3e6bd8c0224af07c501dc63b5f371193
jaymutuku <mutuku.josphat@gmail.com>
2019-09-16 14:53:22 +0300

    write to json file

--- flatten_json_to_csv/main.py
+++ flatten_json_to_csv/main.py
@@ -1,46 +1,61 @@
 import json
-import time
 
 import pandas as pd
 import requests
 
-# from flatsplode import flatsplode
 from flatten_json import flatten
+from pandas.io.json import json_normalize
+
+import pprint 
+pp = pprint.PrettyPrinter(indent=4)
 
 from mytoken import token
 
 # dataframe from csv
-df = pd.read_csv('test_ids.csv',dtype=str)
+df = pd.read_csv("test_ids.csv")
 
 # store results of import
 rows_list = []
 
 headers = {"Authorization": "Bearer " + token}
-url = "https://test-gateway.tulaa.io/cogency-service/credit-info"
+url = "https://test-gateway.tulaa.io/cogency-service/credit-info/"
+
+json_file = 'metropol_test_data.json'
 
 # iterate and get data
 for index, row in df.iterrows():
-    userdata = {
-        "identityNumber": row["identity_number"],
+
+    payload = {
+        "identityNumber": str(row['identity_number']),
         "loanAmount": 6000,
         "reason": 1,
     }
 
-    response = requests.get(url, data=json.dumps(userdata), headers=headers)
-
-    if response.status_code == 200:
-        json_data = rows_list.append(rows_list)
+    response = requests.get(url, params=payload, headers=headers)
+    dict_list = response.json()['data']
+    print(type(dict_list))
+    if response.status_code == 200:        
+        rows_list.append(dict_list)
+        pp.pprint(rows_list)
+        with open(json_file,'w+') as outfile:
+            json.dump(rows_list,outfile)
+            outfile.close()
     else:
         response.raise_for_status()
 
-    print(type(json_data))
+inputFile = open(json_file)
+print(type(inputFile))
 
-    # flatten response
-    flattened = (flatten(d) for d in json_data)
-    # flattened = pd.DataFrame(list(flatsplode(json_data)))
+data = json.load(inputFile)
+print(type(data))
 
-    user_df = pd.DataFrame(flattened)
-    print(user_df)
+flattened = (flatten(d) for d in data)     
+inputFile.close()
 
+print(type(flattened))
+
+dataset = pd.DataFrame(flattened)
+    
+print(dataset)    
     # export to csv
-    user_df.to_csv(str(time.time()) + "metropol_test_data.csv")
+dataset.to_csv('metropol_test_data.csv',index=False)
